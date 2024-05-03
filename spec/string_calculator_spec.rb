4f22872601d0988aeadf58334ed240e1615a3973
Amardeep <amardeep129@gmail.com>
2024-05-03 21:41:38 +0530

    calculator can add any amount of input spec added

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add('1,2')).to eq(3)
     end
+
+		it "returns sum of any amount of number" do
+			calculator = StringCalculator.new
+			expect(calculator.add("1,2,3,4")).to eq(10)
+		end
 	end
 end
