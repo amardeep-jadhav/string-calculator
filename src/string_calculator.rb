ead743442a1d426a3e12f452013c8fd5333a2ac6
Amardeep <amardeep129@gmail.com>
2024-05-03 23:15:05 +0530

    supports different delimiters conditions added

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -1,6 +1,23 @@
 class StringCalculator
 	def add(input)
 		return 0 if input.empty?
-		input.gsub("\n", ',').split(',').map(&:to_i).sum
+
+		if input.start_with?("//")
+			delimiter = input[2] 
+		end
+		
+		if input.include?("\n")
+			numbers = input.gsub("\n", ',').split(',')
+    end
+		
+		if(input.include?("\n") && delimiter)
+			numbers = input.gsub("\n", ',').split(delimiter || ',').last(2)
+		end
+
+		if numbers
+			numbers.join(',').split(',').map(&:to_i).sum
+		else
+			input.split(',').map(&:to_i).sum
+		end
 	end
 end
