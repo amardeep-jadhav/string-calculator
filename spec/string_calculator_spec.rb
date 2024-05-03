b0ba80e5316df078a3377f27adad55e20c7bfa30
Amardeep <amardeep129@gmail.com>
2024-05-03 23:25:55 +0530

    throws an exception for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -31,5 +31,10 @@ RSpec.describe StringCalculator do
 			calculator = StringCalculator.new
 			expect(calculator.add("//;\n1;2")).to eq(3)
 		end
+
+		it 'throws an exception for negative numbers' do
+      calculator = StringCalculator.new
+      expect { calculator.add('1,-2,3,-4') }.to raise_error("Negative numbers not allowed: -2, -4")
+    end
 	end
 end
--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -13,6 +13,11 @@ class StringCalculator
 		if(input.include?("\n") && delimiter)
 			numbers = input.gsub("\n", ',').split(delimiter || ',').last(2)
 		end
+		
+		negative_numbers = input.split(',').map(&:to_i).select { |n| n < 0 }
+		if(negative_numbers.any?)
+			raise "Negative numbers not allowed: #{negative_numbers.join(', ')}"
+		end
 
 		if numbers
 			numbers.join(',').split(',').map(&:to_i).sum
